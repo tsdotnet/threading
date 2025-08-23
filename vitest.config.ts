cca0e8b70881318d74044688ec2be1d92e47876a
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-23 15:28:14 -0700

    reference update

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node',
     globals: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
+    environment: 'node',
+    coverage: {
+      enabled: true,
+      reporter: ['text'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.d.ts'],
+      thresholds: {
+        global: {
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80
+        }
+      }
+    },
   },
 });
